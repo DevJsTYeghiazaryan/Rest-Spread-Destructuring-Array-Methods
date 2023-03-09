bb7533bba3fe88e180e4f41bcc2286cc7cb833a8
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-03-09 20:13:22 +0400

    feat(Create task10.js file)

---
+++ task10.js
@@ -0,0 +1,17 @@
+function filterUsers(users) {
+    return users.filter((user) => user.lang !== "ru");
+  }
+
+  const users = [
+    {
+    username: "Yuri Gagarin",
+    lang: "ru",
+    },
+    {
+    username: "Nil Armstrong",
+    lang: "ENG",
+    },
+];
+
+
+console.log(filterUsers(users)) // [{ username: "Nil Armstrong, lang: "ENG" }]
