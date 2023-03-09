500b2cc6bf8e3e7acd9e9b531edadf9bfca671b5
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-03-09 18:52:21 +0400

    feat(Create index5.js file)

---
+++ task5.js
@@ -0,0 +1,11 @@
+function sortBy(arr, str = "Asc") {
+    if (str === "Asc") {
+      return arr.sort((a, b) => a - b);
+    } else if (str === "Desc") {
+      return arr.sort((a, b) => b - a);
+    } else {
+      return arr;
+    }
+  }
+  
+  console.log(sortBy([4, 3, 2, 1], "Asc"))
