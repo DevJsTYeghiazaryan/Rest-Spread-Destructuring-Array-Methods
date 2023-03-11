071a97645514083370273c3c7154583df93dea6d
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-03-11 22:57:23 +0400

    Changings in findindex.js

---
+++ findindex.js
@@ -0,0 +1,11 @@
+function findIndex(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  const arr = [1, 2, 3, 4, 5];
+  console.log(findIndex(arr, 3)); 
